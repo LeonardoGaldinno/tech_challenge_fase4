27a7c9b5724ae1eca1c9a54cc82403e7fcad686b
LeonardoGaldinno <39771870+LeonardoGaldinno@users.noreply.github.com>
2024-10-26 21:32:27 -0300

    Update Tech_Challenge_Phase_4.py

--- Tech_Challenge_Phase_4.py
+++ Tech_Challenge_Phase_4.py
@@ -15,18 +15,27 @@ st.write("""
 Estrutura dos dados no Big Query
 """)
 
-# Importação dos dados
-project_id = 'neural-journey-377617'
-client = bigquery.Client(project=project_id)
+# Debug statement to check if the code reaches this point
+st.write("Initializing BigQuery client...")
 
-query = """
-SELECT * FROM `neural-journey-377617.cars.cars_table` LIMIT 1000
-"""
-ipea_df = client.query(query).to_dataframe()
+try:
+    # Importação dos dados
+    project_id = 'neural-journey-377617'
+    client = bigquery.Client(project=project_id)
 
-# Dados de Exemplo
-st.header("Demonstração dos Dados (tabelas)")
-st.write(ipea_df.head())
+    query = """
+    SELECT * FROM `neural-journey-377617.cars.cars_table` LIMIT 1000
+    """
+    ipea_df = client.query(query).to_dataframe()
 
-# Exploração de Dados
-st.header("Análise Exploratória de Dados")
+    # Debug statement to check if the query was successful
+    st.write("Query executed successfully. Displaying data...")
+
+    # Dados de Exemplo
+    st.header("Demonstração dos Dados (tabelas)")
+    st.write(ipea_df.head())
+
+    # Exploração de Dados
+    st.header("Análise Exploratória de Dados")
+except Exception as e:
+    st.error(f"An error occurred: {e}")
