b0d813a77269688eac7fa1b8e9db5b6c0664a311
LeonardoGaldinno <39771870+LeonardoGaldinno@users.noreply.github.com>
2024-10-26 21:26:29 -0300

    Update Tech_Challenge_Phase_4.py

--- Tech_Challenge_Phase_4.py
+++ Tech_Challenge_Phase_4.py
@@ -15,22 +15,18 @@ st.write("""
 Estrutura dos dados no Big Query
 """)
 
-# Verificar se a variável de ambiente está definida
-#if 'GOOGLE_APPLICATION_CREDENTIALS' not in os.environ:
-    #st.error("A variável de ambiente GOOGLE_APPLICATION_CREDENTIALS não está definida.")
-#else:
-    # Importação dos dados
-    project_id = 'neural-journey-377617'
-    client = bigquery.Client(project=project_id)
+# Importação dos dados
+project_id = 'neural-journey-377617'
+client = bigquery.Client(project=project_id)
 
-    query = """
-    SELECT  FROM `neural-journey-377617.cars.cars_table` LIMIT 1000
-    """
-    ipea_df = client.query(query).to_dataframe()
+query = """
+SELECT * FROM `neural-journey-377617.cars.cars_table` LIMIT 1000
+"""
+ipea_df = client.query(query).to_dataframe()
 
-    # Dados de Exemplo
-    st.header("Demonstração dos Dados (tabelas)")
-    st.write(ipea_df.head())
+# Dados de Exemplo
+st.header("Demonstração dos Dados (tabelas)")
+st.write(ipea_df.head())
 
 # Exploração de Dados
 st.header("Análise Exploratória de Dados")
